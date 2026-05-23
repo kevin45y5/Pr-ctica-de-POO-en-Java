e2401544f08c59f7465579909ca7cd315bc9b291
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-22 21:08:26 -0600

    KevinMeza Ejercicio 2 terminado

---
+++ src/poo/pagina47/ejercicio3/Ingeniero.java
@@ -0,0 +1,9 @@
+package poo.pagina47.ejercicio3;
+
+public class Ingeniero implements Profesion {
+
+    @Override
+    public void calcularSueldo() {
+        System.out.println("El sueldo del ingeniero es $1200");
+    }
+}
---
+++ src/poo/pagina47/ejercicio3/Main.java
@@ -0,0 +1,11 @@
+package poo.pagina47.ejercicio3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Ingeniero ing = new Ingeniero();
+
+        ing.calcularSueldo();
+    }
+}
